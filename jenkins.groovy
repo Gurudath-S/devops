8f7c09c0408c8cf36aba2cf995a72182f3b83b8c
Gurudath_S <gurudathappu25@gmail.com>
2023-02-26 15:31:06 +0530

    excluded inbuilt tomcat and added jenkins file

---
+++ jenkins.groovy
@@ -0,0 +1,26 @@
+pipeline {
+    agent any
+    
+    stages {
+        stage('Fetch Code') {
+            steps {
+                git 'https://github.com/Gurudath-S/devops.git'
+            }
+        }
+        
+        stage('Build and Test') {
+            steps {
+                sh 'mvn clean install'
+            }
+        }
+        
+        stage('Deploy to Tomcat') {
+            steps {
+                sh '/opt/tomcat/bin/shutdown.sh'
+                sh 'rm -rf /opt/tomcat/webapps/devops*'
+                sh 'cp target/devops.war /opt/tomcat/webapps/'
+                sh '/opt/tomcat/bin/startup.sh'
+            }
+        }
+    }
+}
